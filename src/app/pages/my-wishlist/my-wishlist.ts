e6e529c68c64b7b66715844be990d9c8d19ed12f
Saji <sajjadkazemiswe@gmail.com>
2025-12-06 13:34:46 +0330

    nothing but some tests

--- src/app/pages/my-wishlist/my-wishlist.ts
+++ src/app/pages/my-wishlist/my-wishlist.ts
@@ -1,57 +1,31 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { EcommerceStore } from '../../ecommerce-store';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
-import { BackButton } from '../../components/back-button/back-button';
-import { ProductCard } from '../../components/product-card/product-card';
-import { MatAnchor } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { MatIcon } from "@angular/material/icon";
+import { BackButton } from "../../components/back-button/back-button";
 @Component({
   selector: 'app-my-wishlist',
-  imports: [
-    CommonModule,
-    MatCardModule,
-    MatIcon,
-    BackButton,
-    ProductCard,
-    MatAnchor,
-    RouterLink,
-  ],
+  imports: [CommonModule, MatCardModule, MatIcon, BackButton],
   template: `
     <div class="mx-auto max-w-[1200px] px-4">
-      <app-back-button class="mb-3" navigateTo="/products">{{
-        'back to home' | titlecase
-      }}</app-back-button>
-      <div class="flex justify-between mb-10">
-        <h1 class="text-3xl font-bold">Wishlist</h1>
-        <p class="text-lg">
-          {{ wishlistLen() || 'No' }} Item{{ wishlistLen() > 1 ? 's' : '' }}
-        </p>
+      <app-back-button navigateTo="/products">Back To Home</app-back-button>
+      @for(item of wishlistItems;track item.id;){
+      <div>
+        {{ item.name }}
       </div>
-      @if(wishlistLen() > 0){
-      <div class="responsive-grid">
-        @for(item of this.store.getWishlistItems();track item.id;){
-        <app-product-card [product]="item" [wishlistDeleteButton]="true" />
-        }
-      </div>
-      } @else {
-      <div
-        class="flex flex-col items-center justify-center py-16 text-center gap-5"
-      >
-        <mat-icon class="text-gray-400 transform scale-150"
-          >favorite_border</mat-icon
-        >
-        <div>
-          <h2 class="text-xl font-bold">{{ 'your wishlist is empty' | titlecase }}</h2>
-          <p class="text-gray-600 mb-8 text-lg">
-            {{ 'save your favorite items here' | titlecase }}
+      } @empty {
+      <mat-card class="text-center max-w-[600px] mx-auto" color="">
+        <div class="text-red-500">
+          <mat-icon>priority_high</mat-icon>
+        </div>
+        <div class="text-center text-lg">
+          <h2 class="warning-title">there is no item in your wishlist</h2>
+          <p class="warning-description">
+            Browse items and add them to your wishlist to see them here.
           </p>
         </div>
-        <button matButton="filled" routerLink="/products/all">
-          Explore Products
-        </button>
-      </div>
+      </mat-card>
       }
     </div>
   `,
@@ -59,7 +33,5 @@ import { RouterLink } from '@angular/router';
 })
 export default class MyWishlist {
   store = inject(EcommerceStore);
-  wishlistLen = computed(() => {
-    return this.store.getWishlistItems().length;
-  });
+  wishlistItems = this.store.getWishlistItems();
 }
